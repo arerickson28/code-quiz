5fd371ded0c556b604e8178469b2c3d78c0da9fd
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-05-02 21:10:20 -0500

    added questions object template

---
+++ script.js
@@ -0,0 +1,27 @@
+let quizQuestionObject = {
+
+    "question1": {
+        "options1": ["options1-1", "options1-2", "options1-3", "options1-4"],
+         "answer1": "letter1"
+        },
+    
+    "question2": {
+        "options2": ["options2-1", "options2-2", "options2-3", "options2-4"],
+        "answer2": "letter2"
+            },
+    
+    "question3": {
+        "options3": ["options3-1", "options3-2", "options3-3", "options4-4"],
+        "answer3": "letter3"
+        },
+
+    "question4": {
+        "options4": ["options4-1", "options4-2", "options4-3", "options4-4"],
+        "answer4": "letter4"
+            },
+
+    "question5": {
+        "options5": ["options5-1", "options5-2", "options5-3", "options5-4"],
+        "answer5": "letter5"
+            }
+}
