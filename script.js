adaf98fb57ba69f5554969ad97e6bdf6c103834c
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-05-04 13:39:05 -0500

    reformated question list

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 let quizQuestionArray = [
 
-    {"question1": {
+        {
         "question": "Hello, this is question one!",
-        "options1": ["Hello, this is option one of question one!", "Hello, this is option two of question one!", "Hello, this is option three of question one!", "Hello, this is option four of question one!"],
-         "answer1": "letter1"
-        }},
+        "options": ["Hello, this is option one of question one!", "Hello, this is option two of question one!", "Hello, this is option three of question one!", "Hello, this is option four of question one!"],
+         "answer": "letter1"
+        },
     
-    {"question2": {
+        {
         "question": "Hello, this is question two!",
-        "options2": ["options2-1", "options2-2", "options2-3", "options2-4"],
-        "answer2": "letter2"
-            }},
+        "options": ["options2-1", "options2-2", "options2-3", "options2-4"],
+        "answer": "letter2"
+        },
     
-    {"question3": {
+        {
         "question": "Hello, this is question three!",
-        "options3": ["options3-1", "options3-2", "options3-3", "options4-4"],
-        "answer3": "letter3"
-        }},
+        "options": ["options3-1", "options3-2", "options3-3", "options4-4"],
+        "answer": "letter3"
+        },
 
-    {"question4": {
+        {
         "question": "Hello, this is question four!",
-        "options4": ["options4-1", "options4-2", "options4-3", "options4-4"],
-        "answer4": "letter4"
-            }},
+        "options": ["options4-1", "options4-2", "options4-3", "options4-4"],
+        "answer": "letter4"
+            },
 
-    {"question5": {
+        {
         "question": "Hello, this is question five!",
-        "options5": ["options5-1", "options5-2", "options5-3", "options5-4"],
-        "answer5": "letter5"
-    }}
+        "options": ["options5-1", "options5-2", "options5-3", "options5-4"],
+        "answer": "letter5"
+        }
 ]
-
+//-----------------------------------------------------------------------------
 //Populating question box
+
+let currentQuestion = 0 ;
+
     //Question
 let questionDiv = document.getElementById("questionBox") ;
 
@@ -39,7 +42,7 @@ let nextQuestion = document.createElement("h3") ;
 
 questionDiv.appendChild(nextQuestion) ;
 
-nextQuestion.textContent = quizQuestionArray[0]["question1"]["question"] ;
+nextQuestion.textContent = quizQuestionArray[0]["question"] ;
 
     //Options
 let opA = document.getElementById("op1") ;
@@ -50,9 +53,10 @@ let opD = document.getElementById("op4") ;
 let elOptions = [opA, opB, opC, opD] ;
 
 for (var i = 0; i < 4; i++) {
-    elOptions[i].textContent = quizQuestionArray[0]["question1"]["options1"][i] ;
+    elOptions[i].textContent = quizQuestionArray[currentQuestion]["options"][i] ;
 }
 
+//-----------------------------------------------------------------------------
 
 //At end of quiz...
 let userInitials = "";
@@ -75,25 +79,17 @@ submitInitials.addEventListener("click", function() {
     document.getElementById("userInitials").value = "" ;
 }) ;
 
-console.log(typeof(userInitials)) ;
-
-
-
-//getInitials function?
 
 if (isQuizFinished === false) {
     //TODO: display initial form box
-
-
 }
-
 
 userData = {
     "userInitials" : userInitials,
     "userScore": userScore
 }
 
-
+//-----------------------------------------------------------------------------
 //Connecting startButton to timer
 let startButton = document.getElementById("startButton") ;
 
@@ -101,7 +97,7 @@ startButton.addEventListener("click", function() {
     startTimer() ;
 })
 
-
+//-----------------------------------------------------------------------------
 //Timer stuff
 function startTimer() {
     let timerEl = document.getElementById("timer") ;
