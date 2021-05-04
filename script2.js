f4eedf7fecca2165b36bcbd1036c8ca861819563
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-05-03 21:22:11 -0500

    added clear highscore button functionality

--- script2.js
+++ script2.js
@@ -5,6 +5,8 @@ let highDiv = document.getElementById("highscoreTable") ;
 
 let newUser = document.createElement("tr") ;
 
+newUser.setAttribute("class", "tableRow")
+
 let newUserStat = document.createElement("td") ;
 
 newUserStat.textContent = userInitials ;
@@ -12,6 +14,16 @@ newUserStat.textContent = userInitials ;
 highDiv.appendChild(newUser) ;
 
 newUser.appendChild(newUserStat) ;
+
+// Clear Button
+let clearButton = document.getElementById("clear") ;
+
+clearButton.addEventListener("click", function(){
+    let highscoreTable = document.getElementById("highscoreTable") ;
+    highscoreTable.parentNode.removeChild(highscoreTable) ;
+    // Maybe use "getElementsByClassName"?
+    //https://stackoverflow.com/questions/10842471/how-to-remove-all-elements-of-a-certain-class-from-the-dom
+})
 
 
 
