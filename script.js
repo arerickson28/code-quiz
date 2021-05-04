4fff04f58bb801622cda77ba41b50dfd2f16e51f
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-05-04 12:53:32 -0500

    changed question dictionary to a list of dictionaries

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
-let quizQuestionObject = {
+let quizQuestionArray = [
 
-    "question1": {
+    {"question1": {
+        "question": "Hello, this is question one!",
         "options1": ["options1-1", "options1-2", "options1-3", "options1-4"],
          "answer1": "letter1"
-        },
+        }},
     
-    "question2": {
+    {"question2": {
+        "question": "Hello, this is question two!",
         "options2": ["options2-1", "options2-2", "options2-3", "options2-4"],
         "answer2": "letter2"
-            },
+            }},
     
-    "question3": {
+    {"question3": {
+        "question": "Hello, this is question three!",
         "options3": ["options3-1", "options3-2", "options3-3", "options4-4"],
         "answer3": "letter3"
-        },
+        }},
 
-    "question4": {
+    {"question4": {
+        "question": "Hello, this is question four!",
         "options4": ["options4-1", "options4-2", "options4-3", "options4-4"],
         "answer4": "letter4"
-            },
+            }},
 
-    "question5": {
+    {"question5": {
+        "question": "Hello, this is question five!",
         "options5": ["options5-1", "options5-2", "options5-3", "options5-4"],
         "answer5": "letter5"
-            }
-}
+    }}
+]
+
+//Populating question box
+let questionDiv = document.getElementById("questionBox") ;
+
+let nextQuestion = document.createElement("h3") ;
+
+questionDiv.appendChild(nextQuestion) ;
+
+nextQuestion.textContent = quizQuestionArray[0]["question1"]["question"] ;
+
 
 //At end of quiz...
 let userInitials = "";
